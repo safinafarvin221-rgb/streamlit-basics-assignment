11b749ac5cc2d55b3492c184cb0dc643365d674c
safinafarvin221-rgb <safinafarvin221@gmail.com>
2026-04-02 22:11:56 +0530

    Add files via upload

---
+++ sales_app.py
@@ -0,0 +1,22 @@
+﻿
+import streamlit as st
+import pandas as pd
+
+st.title("Sales Summary App")
+st.subheader("Filter sales by category")
+
+data = {
+    "Product": ["Laptop", "Shirt", "Phone", "Shoes", "Watch"],
+    "Category": ["Electronics", "Clothing", "Electronics", "Clothing", "Accessories"],
+    "Sales": [50000, 2000, 30000, 4000, 7000]
+}
+
+df = pd.DataFrame(data)
+
+category = st.sidebar.selectbox("Select Category", df["Category"].unique())
+
+filtered_df = df[df["Category"] == category]
+
+st.dataframe(filtered_df)
+st.line_chart(filtered_df.set_index("Product")["Sales"])
+
